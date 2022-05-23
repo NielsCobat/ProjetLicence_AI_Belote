418b595f778ef5322c30d835e264430fe0a443d3
ymousel <yael.mousel@etudiant.univ-rennes1.fr>
2022-05-23 13:07:48 +0200

    initialisation terminée. debut des fonctions à implémenter pour changer les inputs au cours de la partie

--- ProjetLicence_AI_Belotte/src/game/NeuralNetwork.java
+++ ProjetLicence_AI_Belotte/src/game/NeuralNetwork.java
@@ -1,4 +1,5 @@
 package game;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import assets.Couleur;
@@ -9,9 +10,9 @@ public class NeuralNetwork extends Joueur{
 	//tableau des entrées et sorties
 
 	/*
-	 * Position:
+	 * Position input:
 	 * 	0 à 31: les cartes en main de l'ia 
-	 * 	32 à 63 : les cartes jouées parr l'ia
+	 * 	32 à 63 : les cartes jouées par l'ia
 	 * 
 	 * 	64 à 95 et 128 à 159 et 192 à 223: les cartes en main des autres joueurs
 	 * 	96 à 127 et 160 à 191 et 224 à 255: les cartes jouées des autres joueurs
@@ -23,12 +24,71 @@ public class NeuralNetwork extends Joueur{
 	 * 
 	 * 	296: 1 si le maître est dans ton équipe
 	 */
-	double[] input = new double[296]; 
-	double[] output = new double[31];
+	double[] input; 
+	
+	/*
+	 * Position output:
+	 * 0 à31: les 32 cartes du jeu parmi lesquelles l'ia va faire son choix
+	 */
+	double[] output;
 
-	//hashMap de reference pour les positions des inputs
+	//hashMap de reference pour les positions des inputs et outputs
 	HashMap<Carte, Integer> posCartesInput = new HashMap<Carte, Integer>();
+	HashMap<Integer,Carte> posCartesOutput = new HashMap<Integer,Carte>();
 
+	int compteurJoueur = 1;
+
+	/*
+	 * Constructeur neural network
+	 */
+	public NeuralNetwork(String nom, int id, int partenaire) {
+		super(nom, id, partenaire);
+		this.main = new ArrayList<Carte>(); // A la creation main forcement vide
+		this.input = new double[296]; 
+		this.output = new double[31];
+	}
+	
+	/*
+	 * Remplit le hashMap output
+	 */
+	void initHashmapOutput() {
+		posCartesOutput.put(0, new Carte(Couleur.Carreau, Valeur.Sept, 0));
+		posCartesOutput.put(1,new Carte(Couleur.Carreau, Valeur.Huit, 0));
+		posCartesOutput.put(2,new Carte(Couleur.Carreau, Valeur.Neuf, 0));
+		posCartesOutput.put(3,new Carte(Couleur.Carreau, Valeur.Dix, 10));
+		posCartesOutput.put(4,new Carte(Couleur.Carreau, Valeur.Valet, 2));
+		posCartesOutput.put(5,new Carte(Couleur.Carreau, Valeur.Dame, 3));
+		posCartesOutput.put(6,new Carte(Couleur.Carreau, Valeur.Roi, 4));
+		posCartesOutput.put(7,new Carte(Couleur.Carreau, Valeur.As, 11));
+		posCartesOutput.put(8,new Carte(Couleur.Coeur, Valeur.Sept, 0));
+		posCartesOutput.put(9,new Carte(Couleur.Coeur, Valeur.Huit, 0));
+		posCartesOutput.put(10,new Carte(Couleur.Coeur, Valeur.Neuf, 0));
+		posCartesOutput.put(11,new Carte(Couleur.Coeur, Valeur.Dix, 10));
+		posCartesOutput.put(12,new Carte(Couleur.Coeur, Valeur.Valet, 2));
+		posCartesOutput.put(13,new Carte(Couleur.Coeur, Valeur.Dame, 3));
+		posCartesOutput.put(14,new Carte(Couleur.Coeur, Valeur.Roi, 4));
+		posCartesOutput.put(15,new Carte(Couleur.Coeur, Valeur.As, 11));
+		posCartesOutput.put(16,new Carte(Couleur.Trefle, Valeur.Sept, 0));
+		posCartesOutput.put(17,new Carte(Couleur.Trefle, Valeur.Huit, 0));
+		posCartesOutput.put(18,new Carte(Couleur.Trefle, Valeur.Neuf, 0));
+		posCartesOutput.put(19,new Carte(Couleur.Trefle, Valeur.Dix, 10));
+		posCartesOutput.put(20,new Carte(Couleur.Trefle, Valeur.Valet, 2));
+		posCartesOutput.put(21,new Carte(Couleur.Trefle, Valeur.Dame, 3));
+		posCartesOutput.put(22,new Carte(Couleur.Trefle, Valeur.Roi, 4));
+		posCartesOutput.put(23,new Carte(Couleur.Trefle, Valeur.As, 11));
+		posCartesOutput.put(24,new Carte(Couleur.Pique, Valeur.Sept, 0));
+		posCartesOutput.put(25,new Carte(Couleur.Pique, Valeur.Huit, 0));
+		posCartesOutput.put(26,new Carte(Couleur.Pique, Valeur.Neuf, 0));
+		posCartesOutput.put(27,new Carte(Couleur.Pique, Valeur.Dix, 10));
+		posCartesOutput.put(28,new Carte(Couleur.Pique, Valeur.Valet, 2));
+		posCartesOutput.put(29,new Carte(Couleur.Pique, Valeur.Dame, 3));
+		posCartesOutput.put(30,new Carte(Couleur.Pique, Valeur.Roi, 4));
+		posCartesOutput.put(31,new Carte(Couleur.Pique, Valeur.As, 11));
+	}
+
+	/*
+	 * Remplit le hashMap input
+	 */
 	void initHashmap() {
 		posCartesInput.put(new Carte(Couleur.Carreau, Valeur.Sept, 0), 0);
 		posCartesInput.put(new Carte(Couleur.Carreau, Valeur.Huit, 0), 1);
@@ -69,13 +129,41 @@ public class NeuralNetwork extends Joueur{
 	 * Initialise le neural network
 	 */
 	void initInput() {
-		
-		//TODO on regarde la main du joueur et on update le init
+
+		//on regarde la main du joueur et on update le init
 		for (Carte carte : this.main) {
-		      input[posCartesInput.get(carte)] = 1;
-		    }
+			input[posCartesInput.get(carte)] = 1;
+		}
+
 
 		//TODO grace à la fonction qui calcule l'ordre des cartes et donc le jeu des autres joueurs onupdate le init de la même manière
+		//solution intermédiaire pour savoir le jeu des autres joueurs
+		if(this.id != 1) {
+			for (Carte carte : Table.joueur1.main) {
+				input[compteurJoueur*64 +posCartesInput.get(carte)] = 1;
+				compteurJoueur++;
+			}
+		}
+		if(this.id != 2) {
+			for (Carte carte : Table.joueur2.main) {
+				input[compteurJoueur*64 +posCartesInput.get(carte)] = 1;
+				compteurJoueur++;
+			}
+		}
+		if(this.id != 3) {
+			for (Carte carte : Table.joueur3.main) {
+				input[compteurJoueur*64 +posCartesInput.get(carte)] = 1;
+				compteurJoueur++;
+			}
+		}
+		if(this.id != 4) {
+			for (Carte carte : Table.joueur4.main) {
+				input[compteurJoueur*64 +posCartesInput.get(carte)] = 1;
+				compteurJoueur++;
+			}
+		}
+		
+		//TODO les cartes sur table
 
 		//init de l'atout
 		switch(Table.atout) {
@@ -93,25 +181,66 @@ public class NeuralNetwork extends Joueur{
 			break;
 		}
 
-		//init de la couleur en jeu
-		//
-		/*switch(Table.atout) {
-		case Carreau:
-			input[288] = 1;
-			break;
-		case Coeur:
-			input[289] = 1;
-			break;
-		case Trefle:
-			input[290] = 1;
-			break;
-		case Pique:
-			input[291] = 1;
-			break;
-		}*/
-		
+		///au debut pas de couleur demandée
+
 		//au debut personne n'est maitre donc le dernier input reste à 0
 	}
 
+	/**
+	 * joue le coup et met à jour les inputs
+	 * @param carte La carte jouée
+	 */
+	void joueCoup(Carte carte) {
+		//calcul de la meilleure carte à jouer
+		int indice = 0;
+		double maxNum = output[0];
+		
+		do {
+			for(int j = 0; j < 32; j++) {
+				if(output[j] > maxNum) {
+					maxNum = output[j];
+					indice = j;
+					// on met l'output à zero pour que si cet output n'est pas légal, qu'il ne soit pas re-selectionné à la boucle suivante
+					output[j] = 0;
+				}
+			}
+		}while((!isLegalMove(posCartesOutput.get(indice))) && (main.contains(posCartesOutput.get(indice))));
 
+
+		super.joueCoup(carte);
+		input[posCartesInput.get(carte)] = 0;
+		input[posCartesInput.get(carte) + 32] = 1;
+	}
+
+	//TODO faire le joueCoup des autres joueurs
+
+	/*
+	 * Met à jour la couleur demandée
+	 */
+	void setCouleurDemandee() {
+		Manche manche = Table.mancheCourante;
+		
+		Couleur couleurEnCours = manche.getPli(manche.getNbPlis()).getCouleurDemandee();
+		
+		switch(couleurEnCours) {
+		case Carreau:
+			input[292] = 1;
+			break;
+		case Coeur:
+			input[293] = 1;
+			break;
+		case Trefle:
+			input[294] = 1;
+			break;
+		case Pique:
+			input[295] = 1;
+			break;
+		}
+	}
+	
+	//TODO si le partenaire est maitre ou non
+
+	//TODO un reset du pli pour lescartes sur table
+
+	//TODO un reset de l'ia quand on change de manche
 }
